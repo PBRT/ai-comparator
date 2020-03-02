08745b9de13d1670ffb454eb139959b61ddb98da
PBRT <pierre.beard@workangel.com>
2020-03-02 16:31:39 -0500

    feat(compare): improve layout of comparison view

--- src/containers/compare-agents/CompareAgentsContainer.tsx
+++ src/containers/compare-agents/CompareAgentsContainer.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { IconNames } from "@blueprintjs/icons";
+import { Spinner, Icon, Intent } from "@blueprintjs/core";
 
 import { TEST_IDS } from "../../tests/test-ids";
+import SectionHeader from "../../components/section-header/SectionHeader";
 
 function CompareAgentsContainer() {
+  function refresh() {
+    console.log("todo");
+  }
   return (
     <div data-testid={TEST_IDS.CONTAINER_COMPARISON_ROOT}>
-      Compare Agents Container
+      <SectionHeader
+        title="AI Comparator"
+        subtitle="Visualize the performances differences between two agents."
+        actions={[
+          {
+            action: refresh,
+            label: "Refresh",
+            icon: IconNames.REFRESH,
+            tooltipText: "Re-fresh to get the latest data.",
+            disabled: false,
+            intent: Intent.NONE
+          }
+        ]}
+      />
     </div>
   );
 }
